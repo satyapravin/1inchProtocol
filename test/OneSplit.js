d566151172cfacf5d2d979456c7cb0fbd7b55a31
pavy_bez <56443544+satyapravin@users.noreply.github.com>
2020-08-04 14:12:18 +0800

    Update OneSplit.js

--- test/OneSplit.js
+++ test/OneSplit.js
@@ -256,21 +256,21 @@ contract('OneSplit', function ([_, addr1]) {
             console.log('raw:', res.returnAmount.toString());
         });
         
-        it.only('should work with Mooniswap ETH => DAI', async function () {
-            const res = await this.split.getExpectedReturn(
-                '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE', // ETH
-                '0x6B175474E89094C44Da98b954EedeAC495271d0F', // DAI
-                '1000000000000000000', // 1.0
-                10,
-                DISABLE_ALL.add(MOONISWAP_ALL), // enable only Mooniswap
-            );
+        //it.only('should work with Mooniswap ETH => DAI', async function () {
+        //    const res = await this.split.getExpectedReturn(
+        //        '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE', // ETH
+        //        '0x6B175474E89094C44Da98b954EedeAC495271d0F', // DAI
+        //        '1000000000000000000', // 1.0
+        //        10,
+        //        DISABLE_ALL.add(MOONISWAP_ALL), // enable only Mooniswap
+        //    );
 
-            console.log('Swap: 1 ETH');
-            console.log('returnAmount:', res.returnAmount.toString() / 1e18 + ' DAI');
-            // console.log('distribution:', res.distribution.map(a => a.toString()));
-            // console.log('raw:', res.returnAmount.toString());
-            expect(res.returnAmount).to.be.bignumber.above('20999999969');
-        });
+        //    console.log('Swap: 1 ETH');
+        //    console.log('returnAmount:', res.returnAmount.toString() / 1e18 + ' DAI');
+        //    // console.log('distribution:', res.distribution.map(a => a.toString()));
+        //    // console.log('raw:', res.returnAmount.toString());
+        //    expect(res.returnAmount).to.be.bignumber.above('20999999969');
+        //});
 
     });
 });
